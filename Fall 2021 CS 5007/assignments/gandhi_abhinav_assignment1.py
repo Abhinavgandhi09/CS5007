ccdd6fcbabfc927e505f86aa08c6745435bc6f5a
Abhinav Gandhi <abhinavgandhi09@gmail.com>
2021-09-22 07:13:18 -0400

    Wrote better code

--- Fall 2021 CS 5007/assignments/gandhi_abhinav_assignment1.py
+++ Fall 2021 CS 5007/assignments/gandhi_abhinav_assignment1.py
@@ -1,85 +1,89 @@
 
+def compute_student_statistics(exam_scores, exam_count, student_num, result):
+    # Computing avg score
+    score_sum = 0
+    for score in exam_scores:
+        score_sum += score
+    try:
+        avg_score = score_sum/exam_count
+    except ZeroDivisionError:
+        avg_score = 0
+
+    # Computing highest score
+    try:
+        max_score = max(exam_scores)
+    except ValueError:
+        max_score = 0
+
+    # Computing lowest score
+    try:
+        min_score = min(exam_scores)
+    except ValueError:
+        min_score = 0
+
+    result = result + "Student " + str(student_num) + " took " + str(exam_count) + " exams" + "\n"
+    result = result + "Average score: " + str(round(avg_score, 2)) + "\n"
+    result = result + "Highest score: " + str(max_score) + "\n"
+    result = result + "Lowest score: " + str(min_score) + "\n" + "\n"
+    return result
+
 
 def main():
-
     # ------------------------- Declaring required variables ----------------------
     student = True  # variable to check if there are any more students
-    count = 1  # variable to count number of students
-    score_lst = []  # list to store student scores
+    student_count = 0  # variable to count number of students
     score = 0  # variable to store input score
-    score_count = 0  # variable to count number of scores per student
-    n_exam = []  # list to store number of exams per student
-    s = ""  # stores string entered by user
-    offset = 0  # variable to iterate through scores list
-    max_score = 0  # variable to store Highest score
-    min_score = 10000  # variable to store Lowest score
-    score_sum = 0  # variable to store sum of a students scores
+    flag = True  # input validation flag
+    str_result = ""  # stores results
 
-    # ------------------------- Prompt user for details and store ----------------------
+    # -------- Prompt user for student details, compute student's statistics --------
+
     while student:
+        # Declare score_lst & score_count variables for each student
+        score_lst = []
+        score_count = 0
+
+        student_count += 1  # Increase student count
+
+        # Receive student's score from user input
+        try:
+            score = float(input("Please enter student " + str(student_count) + "\'s score (-1: Exit): "))
+        except ValueError:
+            print("The score entered is not a number. Please enter it again.")
+            print()
+            flag = False
+
         while score != -1:
 
-            # Receive student's score and append to list only if it is not -1
+            if flag:  # Ensuring score is valid, if not prompt user again
+
+                # Append score to list
+                score_lst.append(score)
+                score_count += 1  # Accumulator to count number of exams
+
+            # Receive student's score from user input
             try:
-                score = float(input("Please enter student "+str(count)+"\'s score (-1: Exit): "))
+                score = float(input("Please enter student  " + str(student_count) + "\'s score (-1: Exit): "))
+                flag = True
             except ValueError:
                 print("The score entered is not a number. Please enter it again.")
-                score = float(input("Please enter student "+str(count)+"\'s score (-1: Exit): "))
+                print()
+                flag = False
 
-            if score != -1:
-                score_lst.append(score)
-                score_count += 1  # count number of exams
-
-        # Store the number of exams to the list n_exam
-        n_exam.append(score_count)
-
-        # Reset score & score_count for next student
-        score_count = 0
-        score = 0
+        # Compute student's statistics and store as string
+        str_result = compute_student_statistics(score_lst, score_count, student_count, str_result)
 
         # Check whether any more students exist
         print()
         s = input("Any more student?(Yes or No): ")
         print()
-        if s == "Yes":
+        if s.upper() == "YES":
             student = True
         else:
             student = False
 
-        # Increase student count if student exists
-        if student:
-            count += 1
-
-    # ------------------------------ Compute and print results -----------------------
-    for i in range(1, count+1, 1):  # Here i is student number
-        print("Student ", i, "took ", (n_exam[i - 1]), " exams")
-
-        for j in range(n_exam[i - 1]):
-            score_sum = score_sum + score_lst[offset + j]  # sum of student i's scores
-
-            if score_lst[offset + j] > max_score:  # Highest score of student i
-                max_score = score_lst[offset + j]
-
-            if score_lst[offset + j] < min_score:  # Lowest score of student i
-                min_score = score_lst[offset + j]
-
-        offset += n_exam[i - 1]  # index offset for next student's scores
-        # This accounts for the offsets due to the '-1' in the list
-        try:
-            avg = 1.0*score_sum/(n_exam[i - 1])  # avg score for student i
-        except ZeroDivisionError:
-            avg = 0
-            min_score = 0
-
-        print("Average score: ", round(avg, 2))
-        print("Highest score: ", max_score)
-        print("Lowest score: ", min_score)
-        print()
-
-        # Reset max and min score and score sum
-        max_score = 0
-        min_score = 10000
-        score_sum = 0
+    # Print results
+    print(str_result)
 
 
 if __name__ == "__main__":
